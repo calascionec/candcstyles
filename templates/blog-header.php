5a4b0f2af5fffdcd6ff994c3ed802e9d13486840
Chris <Ccalascione@riversagency.com>
2016-08-10 20:11:09 -0400

    update styles for nav and dropdown

--- templates/blog-header.php
+++ templates/blog-header.php
@@ -10,27 +10,61 @@
 
         </div><!--blog-heading-->
 
-        <div class="navigation-blog">
-            <a class="navigation-blog-link" href="#"><i class="icon-book"></i> Education</a>
-            <a class="navigation-blog-link" href="#"><i class="icon-ring"></i> Design</a>
-            <a class="navigation-blog-link" href="#"><i class="icon-globe"></i> Social Responsibility</a>
-            <a class="navigation-blog-link" href="#"><i class="icon-plus"></i> Archives</a>
-
-            <!-- <div class="archive-dropdown">
-                <a href="#">2016 <i class="icon-plus"></i></a>
-                <ul>
-                    <li><a href="#">August</a></li>
-                    <li><a href="#">July</a></li>
-                    <li><a href="#">June</a></li>
-                    <li><a href="#">May</a></li>
-                    <li><a href="#">April</a></li>
-                    <li><a href="#">March</a></li>
-                    <li><a href="#">February</a></li>
-                    <li><a href="#">January</a></li>
+        <ul class="navigation-blog">
+            <li class="menu-item">
+                <a class="menu-item-link" href="#"><i class="icon-book"></i> Education</a>
+            </li>
+            <li class="menu-item">
+                <a class="menu-item-link" href="#"><i class="icon-ring"></i> Design</a>
+            </li>
+            <li class="menu-item">
+                <a class="menu-item-link" href="#"><i class="icon-globe"></i> Social Responsibility</a>
+            </li>
+            <li class="menu-item">
+                <a class="menu-item-link" href="#"><i class="icon-plus"></i> Archives</a>
+                <ul class="sub-menu">
+                    <li>
+                        <a href="#">2016 <i class="icon-plus"></i></a>
+                        <ul class="sub-dropdown">
+                            <li><a href="#">August</a></li>
+                            <li><a href="#">July</a></li>
+                            <li><a href="#">June</a></li>
+                            <li><a href="#">May</a></li>
+                            <li><a href="#">April</a></li>
+                            <li><a href="#">March</a></li>
+                            <li><a href="#">February</a></li>
+                            <li><a href="#">January</a></li>
+                        </ul>
+                    </li>
+                    <li>
+                        <a href="#">2016 <i class="icon-plus"></i></a>
+                        <ul>
+                            <li><a href="#">August</a></li>
+                            <li><a href="#">July</a></li>
+                            <li><a href="#">June</a></li>
+                            <li><a href="#">May</a></li>
+                            <li><a href="#">April</a></li>
+                            <li><a href="#">March</a></li>
+                            <li><a href="#">February</a></li>
+                            <li><a href="#">January</a></li>
+                        </ul>
+                    </li>
+                    <li>
+                        <a href="#">2016 <i class="icon-plus"></i></a>
+                        <ul>
+                            <li><a href="#">August</a></li>
+                            <li><a href="#">July</a></li>
+                            <li><a href="#">June</a></li>
+                            <li><a href="#">May</a></li>
+                            <li><a href="#">April</a></li>
+                            <li><a href="#">March</a></li>
+                            <li><a href="#">February</a></li>
+                            <li><a href="#">January</a></li>
+                        </ul>
+                    </li>
                 </ul>
-
-            </div> -->
-        </div>
+            </li>
+        </ul><!--navigation-blog-->
 
     </div><!--cc-container-->
 
